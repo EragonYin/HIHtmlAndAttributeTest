d1b3b6f8b5c73e267ce48ef924b718a80dee93ec
HIChen <295098309@qq.com>
2016-07-22 17:04:10 +0800

    Initial Commit

---
+++ HIHTMLEditTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HIHTMLEditTest
+//
+//  Created by HIChen on 16/7/22.
+//  Copyright © 2016年 风聆小镇工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HIHTMLEditTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HIHTMLEditTest
+//
+//  Created by HIChen on 16/7/22.
+//  Copyright © 2016年 风聆小镇工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
